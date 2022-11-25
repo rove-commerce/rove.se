ac556218d38bf6ccd8b91fa42f165a1bd1758752
Henrik Lundin <henke.lundin@gmail.com>
2022-11-25 16:49:52 +0100

    GA router

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,9 +1,13 @@
 import VueGtag from "vue-gtag";
 
 export default defineNuxtPlugin((nuxtApp) => {
-	nuxtApp.vueApp.use(VueGtag, {
-		property: {
-			id: "G-S99EGMCMMG",
+	nuxtApp.vueApp.use(
+		VueGtag,
+		{
+			config: {
+				id: "G-S99EGMCMMG",
+			},
 		},
-	});
+		nuxtApp.$router
+	);
 });
